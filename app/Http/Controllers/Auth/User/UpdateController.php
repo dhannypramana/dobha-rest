46abedfa5f013a780fb8c71eb235d2c434b30505
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-04-07 00:44:57 +0700

    fix problem: another user now cant update some user data

--- app/Http/Controllers/Auth/User/UpdateController.php
+++ app/Http/Controllers/Auth/User/UpdateController.php
@@ -16,6 +16,12 @@ class UpdateController extends Controller
      */
     public function __invoke(Request $request, User $user)
     {
+        if (auth()->user()->username !== $user->username) {
+            return response()->json([
+                'message' => 'unauthorized'
+            ]);
+        }
+
         $request->validate([
             'name' => 'required|min:3|max:25',
             'email' => 'required|min:3|max:25',
