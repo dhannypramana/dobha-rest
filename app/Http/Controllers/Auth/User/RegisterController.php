b2aa10bcc4564de25562c177200a5709f5c8e822
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-04-14 09:52:38 +0700

    err register

--- app/Http/Controllers/Auth/User/RegisterController.php
+++ app/Http/Controllers/Auth/User/RegisterController.php
@@ -17,32 +17,25 @@ class RegisterController extends Controller
      */
     public function __invoke(Request $request)
     {
-        try {
-            $request->validate([
-                'name' => 'required|min:3|max:25',
-                'username' => 'required|min:3|max:25|unique:users,username',
-                'email' => 'required|min:3|max:25|unique:users,email',
-                'password' => 'required|min:8|max:20|',
-                'phone_number' => 'required|min:11'
-            ]);
-    
-            $user = User::create([
-                'name' => $request->name,
-                'username' => $request->username,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-                'phone_number' => $request->phone_number
-            ]);
-    
-            return response()->json([
-                'message' => 'registration success',
-                'user' => $user,
-            ]);    
-        } catch (Exception $e) {
-            return response()->json([
-                'error' => $e->getMessage(),
-                'message' => 'data tidak valid'
-            ], 422);
-        }
+        $request->validate([
+            'name' => 'required|min:3|max:25',
+            'username' => 'required|min:3|max:25|unique:users,username',
+            'email' => 'required|min:3|max:25|unique:users,email',
+            'password' => 'required|min:8|max:20|',
+            'phone_number' => 'required|min:11'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'phone_number' => $request->phone_number
+        ]);
+
+        return response()->json([
+            'message' => 'registration success',
+            'user' => $user,
+        ]);    
     }
 }
