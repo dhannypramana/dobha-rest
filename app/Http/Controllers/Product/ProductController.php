c49814e94c2121afc35124d823c79bf3ec737cb3
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-05-19 00:04:20 +0700

    change searh method pgsql

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -29,7 +29,7 @@ class ProductController extends Controller
     {
         if ($request->has('q')) {
             $request = strtolower($request->q);
-            $product = Product::where('nama_produk', 'like', "%" . $request . "%")->paginate(5)->withQueryString();
+            $product = Product::where('nama_produk', 'ILIKE', '%' . trim($parameters['request']) . '%')->paginate(5)->withQueryString();
             return $product;
         }
 
