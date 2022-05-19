390d1a497a7fc3d0952ce023ee7bdcee6738f9e0
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-05-19 22:31:29 +0700

    update font form reset password

--- resources/views/form-reset-password.blade.php
+++ resources/views/form-reset-password.blade.php
@@ -9,6 +9,16 @@
     <meta name="author" content="">
 
     <title>Reset Password</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        * {
+            font-family: 'Quicksand', sans-serif;
+        }
+    </style>
 
     <!-- Custom fonts for this template-->
     <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
